364f2a35e53193a8f6bbd8c7ece412886c70b2f4
罗衍修 <a18736847492@163.com>
2019-06-10 15:44:42 +0800

    罗衍修提交order和order_line表的controller和server的编码

--- src/main/java/com/briup/apps/ej/service/impl/OrderServiceImpl.java
+++ src/main/java/com/briup/apps/ej/service/impl/OrderServiceImpl.java
@@ -1,4 +1,72 @@
 package com.briup.apps.ej.service.impl;
 
-public class OrderServiceImpl {
+import com.briup.apps.ej.bean.Order;
+import com.briup.apps.ej.bean.OrderExample;
+import com.briup.apps.ej.dao.OrderMapper;
+import com.briup.apps.ej.service.OrderService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Override
+    public long countByExample(OrderExample example) {
+        return  orderMapper.countByExample(example);
+    }
+
+    @Override
+    public int deleteByExample(OrderExample example) {
+        return orderMapper.deleteByExample(example);
+    }
+
+    @Override
+    public int deleteByPrimaryKey(Long id) {
+        return orderMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insert(Order record) {
+        return orderMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(Order record) {
+        return orderMapper.insertSelective(record);
+    }
+
+    @Override
+    public List<Order> selectByExample(OrderExample example) {
+        return orderMapper.selectByExample(example);
+    }
+
+    @Override
+    public Order selectByPrimaryKey(Long id) {
+        return orderMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateByExampleSelective(Order record, OrderExample example) {
+        return orderMapper.updateByExampleSelective(record,example);
+    }
+
+    @Override
+    public int updateByExample(Order record, OrderExample example) {
+        return orderMapper.updateByExample(record, example);
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(Order record) {
+        return orderMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int updateByPrimaryKey(Order record) {
+        return orderMapper.updateByPrimaryKey(record);
+    }
 }
