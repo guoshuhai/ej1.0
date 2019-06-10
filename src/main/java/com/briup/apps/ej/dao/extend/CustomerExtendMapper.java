e17eebfb21a44da760bee9e9e8ca47f8527c4f5a
weisheng <112811>
2019-06-10 18:17:14 +0800

    李帅 更新第一次

--- src/main/java/com/briup/apps/ej/dao/extend/CustomerExtendMapper.java
+++
@@ -1,9 +0,0 @@
-package com.briup.apps.ej.dao.extend;
-import com.briup.apps.ej.bean.extend.CommentExtend;
-
-import java.util.List;
-public interface CustomerExtendMapper {
-
-    List<CommentExtend> selectAll();
-
-}
