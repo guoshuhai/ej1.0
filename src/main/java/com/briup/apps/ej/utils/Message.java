33c9e5e5463e2789eedd10a0c3187b5aa7d8275b
guoshuhai <780931703@qq.com>
2019-06-17 16:58:46 +0800

    修改数据库密码

--- src/main/java/com/briup/apps/ej/utils/Message.java
+++ src/main/java/com/briup/apps/ej/utils/Message.java
@@ -1,4 +1,7 @@
 package com.briup.apps.ej.utils;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Message {
     private Integer status;
@@ -36,5 +39,12 @@ public class Message {
 
     public void setTimestamp(Long timestamp) {
         this.timestamp = timestamp;
+//        long timeStamp = System.currentTimeMillis();  //获取当前时间戳,也可以是你自已给的一个随机的或是别人给你的时间戳(一定是long型的数据)
+//        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//这个是你要转成后的时间的格式
+//        String sd = sdf.format(new Date(timeStamp));   // 时间戳转换成时间
+//        System.out.println(sd);//打印出你要的时间
+        //  结果就是: 2017-05-26 13:42:15
+
+
     }
 }
